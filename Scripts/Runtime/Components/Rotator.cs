088c16266e3e0c9c17bd31e547f8afc1f9d445ce
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2023-03-13 19:41:39 +0100

    insert simple animation scaler and rotator

---
+++ Scripts/Runtime/Components/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityAnimation.Runtime.animation.Scripts.Runtime.Types;
+using UnityEngine;
+
+namespace UnityAnimation.Runtime.animation.Scripts.Runtime.Components
+{
+    [AddComponentMenu(UnityAnimationConstants.Root + "/Rotator")]
+    [DisallowMultipleComponent]
+    public sealed class Rotator : SimpleAnimator
+    {
+        #region Inspector Data
+
+        [SerializeField]
+        private AnimationCurve rotationCurve = AnimationCurve.Constant(0f, 1f, 1f);
+
+        [SerializeField]
+        private Vector3 eulerRotation;
+
+        #endregion
+
+        protected override void Animate(float deltaTime) =>
+            transform.rotation *= Quaternion.Euler(eulerRotation * (deltaTime * rotationCurve.Evaluate(deltaTime)));
+    }
+}
--- Scripts/Runtime/UnityAnimationConstants.cs
+++ Scripts/Runtime/UnityAnimationConstants.cs
@@ -2,6 +2,6 @@ namespace UnityAnimation.Runtime.animation.Scripts.Runtime
 {
     internal static class UnityAnimationConstants
     {
-        
+        public const string Root = "Unity Animations";
     }
 }
